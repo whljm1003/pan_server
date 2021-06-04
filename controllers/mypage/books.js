10c3c6bd8f7ab574505595d7d9bdc278606712e9
Jiyoung Moon <jiyoungmoon@Jiyoungui-MacBookPro.local>
2021-06-05 00:09:00 +0900

    feature/14/books GET modified

--- controllers/mypage/books.js
+++ controllers/mypage/books.js
@@ -1,6 +1,6 @@
 //작성자:문지영
 const sequelize = require("sequelize");
-const { User, Book } = require('../../models');
+const { User, Book, Diary, Like } = require('../../models');
 const jwt = require('jsonwebtoken');
 
 module.exports = {
@@ -29,7 +29,7 @@ module.exports = {
             bookName: bookName,
             bookCover: bookCover,
         })
-        res.status(200).json({ message: '일기장이 생성되었습니다.' })
+        res.status(200).json({ message: '개인 일기장이 생성되었습니다.' })
 
         //그룹 일기장 생성(groupId가 있을 때), //user가 group에 속해있으면 groupdId가 있음
     },
@@ -46,21 +46,22 @@ module.exports = {
         const token = authorization.split(' ')[1];
         const data = jwt.verify(token, process.env.ACCESS_SECRET);
 
-        const userInfo = await User.findOne({ where: { id: data.id } });
-
         //mypage에서 내가 생성한 개인일기장 목록 보기(로그인 회원만 볼 수 있음)
         const book = await Book.findAll({
             where: { userId: data.id },
             attributes: ['userId', 'groupId', 'bookName', 'bookCover'],
-            // include: [{
-            //     model: Diary,
-            //     required: false,
-            //     attributes: ['title', 'weather', 'content', 'date', 'feelings', 'picUrl', 'bookId'],
-            //     where: { userId: data.id}
-            // }]
+            include: [{
+                model: Diary,
+                required: false,
+                attributes: ['userId', 'bookId', 'type', 'title', 'weather', 'content', 'private', 'picUrl', 'date', 'feelings'],
+                include: [{
+                    model: Like,
+                    required: false,
+                    attributes: ['like']
+                }]
+            }]
         });
-        // console.log(book)
-        res.status(200).json({ data: book, message: '개인 일기장 목록입니다.' });
+        res.status(200).json({ data: book, message: '개인 일기장 및 일기 목록입니다.' });
     },
 
     put: async (req, res) => {
