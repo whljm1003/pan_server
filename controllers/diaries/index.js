978dfb5e28ace4cb6e8b0139e5ea9d387da041e4
Jiyoung Moon <jiyoungmoon@Jiyoungui-MacBookPro.local>
2021-06-09 17:27:45 +0900

    feature/17/search merge conflict modified..

--- controllers/diaries/index.js
+++ controllers/diaries/index.js
@@ -4,8 +4,5 @@ module.exports = {
     diaryList: require('./diaryList'),
     groupDiaryList: require('./groupDiaryList'),
     trending: require('./trending'),
-<<<<<<< HEAD
     search: require('./search')
-=======
->>>>>>> 238062d95f0ae55b73a038f20fca5fd4e9ed89ba
 };
--- routes/diaries.js
+++ routes/diaries.js
@@ -7,11 +7,7 @@ const {
     diaryList,
     groupDiaryList,
     trending,
-<<<<<<< HEAD
     search
-=======
->>>>>>> 238062d95f0ae55b73a038f20fca5fd4e9ed89ba
-
 } = require('../controllers/diaries');
 
 // * POST /diaries/upload
